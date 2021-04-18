0ebd0f2a122fee8bb18a521ae0aeb857083d9194
lemonka <skiba.adam@gmail.com>
2021-04-18 12:47:14 +0200

    Update main.py

--- pulseaudio_fix/main.py
+++ pulseaudio_fix/main.py
@@ -1,43 +1,23 @@
 import subprocess
 
 
-def is_loaded():
-    """This function determines if the module `module-suspend-on-idle` is loaded """
-    proc = subprocess.Popen(["pactl", "list", "short"], stdout=subprocess.PIPE)
-    stdout = proc.communicate()[0].decode("UTF-8")
-    if "module-suspend-on-idle" in stdout:
-        return True
-    else:
-        return False
 
 
 def load_module():
     """This function loads the module and returns any errors that occur in the process."""
-    proc = subprocess.Popen(["pactl", "load-module", "module-suspend-on-idle"], stderr=subprocess.PIPE)
-    stderr = proc.communicate()[1].decode("UTF-8")
-    return stderr
-
-
-def unload_module():
-    """This function unloads the module and returns any errors that occur in the process."""
-    proc = subprocess.Popen(["pactl", "unload-module", "module-suspend-on-idle"], stderr=subprocess.PIPE)
+    proc = subprocess.Popen(["pactl", "set-default-sink", "alsa_output.usb-3DSA_USB_SOUND_3DSA_USB_SOUND_23456789-00.analog-stereo"], stderr=subprocess.PIPE)
     stderr = proc.communicate()[1].decode("UTF-8")
     return stderr
 
 
 def main():
     """Main function"""
-    if is_loaded():
-        err = unload_module()
-        if len(err) > 0:
-            raise Exception("[ALSA&PULSEAUDIO FIX][ERROR] Error unloading module `module-suspend-on-idle`!"
-                            "Output pactl: {}".format(err))
     err = load_module()
     if len(err) > 0:
-        Exception("[ALSA&PULSEAUDIO FIX][ERROR] Error loading module `module-suspend-on-idle`!"
+        Exception("[ALSA&PULSEAUDIO FIX][ERROR] Error settingdefault card`!"
                   "Output pactl: {}".format(err))
     else:
-        print("[ALSA&PULSEAUDIO FIX][INFO] Module `module-suspend-on-idle` loaded successfully!")
+        print("[ALSA&PULSEAUDIO FIX][INFO] Default card set!")
 
 
 if __name__ == '__main__':
